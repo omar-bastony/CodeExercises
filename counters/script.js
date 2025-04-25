66ddce51a8e23dcbc0f1a9085b2eb00f6880aa20
omar-bastony <bastonyomar@gmail.com>
2025-04-25 17:30:02 +0300

    Using Closure

--- counters/script.js
+++ counters/script.js
@@ -1,8 +1,17 @@
-document.querySelectorAll('.inc').forEach(btn => {btn.addEventListener('click', () => {
-    btn.parentElement.querySelector('.count').innerText++;
+document.querySelectorAll('.inc').forEach((btn, idx) => {btn.addEventListener('click', () => {
+    btn.parentElement.querySelector('.count').innerText = setCounter[idx].increaseFunction();
 })});
 
-document.querySelectorAll('.dec').forEach(btn => {btn.addEventListener('click', () => {
-    let count = btn.parentElement.querySelector('.count');
-    if (count.innerText != 0) count.innerText--; 
+document.querySelectorAll('.dec').forEach((btn, idx) => {btn.addEventListener('click', () => {
+    btn.parentElement.querySelector('.count').innerText = setCounter[idx].decreaseFunction();
 })});
+
+function changeCounter(num) {
+    let counter = num;
+    return {
+        increaseFunction() { return ++counter },
+        decreaseFunction() { return --counter },
+    }
+}
+
+const setCounter = [changeCounter(0), changeCounter(10), changeCounter(100)];
